89e395aed3b169759c26c082f094fd49983c6681
chuckgstanley <cgs2138@columbia.edu>
2014-02-25 20:31:59 -0500

    wet up pageloaded function

---
+++ scripts.js
@@ -0,0 +1,18 @@
+/**
+ * @author Peterphobia
+ */
+
+//make sure html is pulling jscripts
+console.log("javascript working")
+
+
+
+function pageLoaded() {
+	
+	//indicate page has loaded
+	console.log("Page Loaded!");
+	
+	
+}
+// pageLoaded function
+$(document).ready(pageLoaded);
